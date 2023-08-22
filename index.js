6603fee165d2f9bbe6ae923b5808a9393078e4de
Rafaelbalthar <134559014+Rafaelbalthar@users.noreply.github.com>
2023-08-21 21:43:35 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+
+//Isso é um comentário 
+
+/*
+Um comentário é ignorado no momento em que o programa 
+*/
+
+console.log ("Olá Mundo");
+
+//prompt("Qual é o seu nome?");
+
+
+//Variáveis
+const variavelA = 100;
+const variavelB = variavelA + 50;
+console.log (variavelB);
+
+let numero = 50 
+console.log (numero);
+numero = 80;
+console.log (numero)
+
+/*const nome = "Fulana";
+const idade = 21;
+*/
+
+//console.log ("Olá!", "Meu nome é", nome, "e eu tenho", idade, "anos");
+
+// Nome
+let nome = "Carlos";
+
+// Sobrenome
+let sobrenome = "Passos";
+
+// Idade
+let idade = 22;
+
+// Estudande ou não
+let estudante = true;
+console.log ("Meu é",nome,sobrenome,"e tenho",idade,"anos.","Sou estudante?",estudante);
